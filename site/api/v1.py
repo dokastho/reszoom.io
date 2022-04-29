d43bf916b0484a63bc4d264b9e12520066bc5f3e
Thomas Dokas <dokastho@umich.edu>
2022-04-29 11:55:12 -0400

    imported /api/v1 route

---
+++ site/api/v1.py
@@ -0,0 +1,16 @@
+"""REST API for other routes."""
+import flask
+import site
+
+
+@site.app.route('/api/v1/', methods=['GET'])
+def get_routes():
+    """Return all possible REST API routes."""
+    return flask.jsonify({
+        # TODO: establish some routes to service
+
+        # "comments": "/api/v1/comments/",
+        # "likes": "/api/v1/likes/",
+        # "posts": "/api/v1/posts/",
+        # "url": "/api/v1/"
+    }), 200
