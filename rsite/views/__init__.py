374444e86a597e060981a4857576f2e92a167501
Thomas Dokas <dokastho@umich.edu>
2022-05-01 00:24:16 -0400

    build-2022.05.01_00.24.16-tmp2

--- rsite/views/__init__.py
+++ rsite/views/__init__.py
@@ -1,10 +1,6 @@
 """Views of site server-side dynamic project."""
-# example code pasted from p3
+
 from rsite.views.user import show_user
-# from insta485.views.posts import show_post, posts
-# from insta485.views.likes import likes
-# from insta485.views.comments import comments
-# from insta485.views.uploads import get_image
-# from insta485.views.explore import show_explore
 from rsite.views.index import show_index
 from rsite.views.accounts import login, accounts
+from rsite.views.resume import show_resume
