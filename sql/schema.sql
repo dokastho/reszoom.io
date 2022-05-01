2511df6525400405ca5c61d334f865201f29a108
Thomas Dokas <dokastho@umich.edu>
2022-05-01 16:20:36 -0400

    added new schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -9,3 +9,31 @@ CREATE TABLE users(
   created DATETIME DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY(username)
 );
+
+CREATE TABLE resumes(
+  resumeid INTEGER PRIMARY KEY AUTOINCREMENT,
+  owner VARCHAR(20) NOT NULL,
+  typename BOOLEAN NOT NULL,
+  snapshot VARCHAR(64) NOT NULL,
+  FOREIGN KEY(owner) REFERENCES users(username) ON DELETE CASCADE
+);
+
+CREATE TABLE entries(
+  resumeid INTEGER NOT NULL,
+  entryid INTEGER PRIMARY KEY AUTOINCREMENT,
+  content VARCHAR(256),
+  FOREIGN KEY(resumeid) REFERENCES resumes(resumeid) ON DELETE CASCADE
+);
+
+CREATE TABLE tags(
+  tagid INTEGER PRIMARY KEY AUTOINCREMENT,
+  tagname VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE entries_to_tags(
+  entryid INTEGER NOT NULL,
+  tagid INTEGER NOT NULL,
+  PRIMARY KEY(entryid, tagid),
+  FOREIGN KEY(entryid) REFERENCES entries(entryid) ON DELETE CASCADE,
+  FOREIGN KEY(tagid) REFERENCES tags(tagid) ON DELETE CASCADE
+);
