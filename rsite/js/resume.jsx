ac067e1d12a8f430912b1868f9f3f6b1859848eb
Thomas Dokas <dokastho@umich.edu>
2022-05-01 21:41:54 -0400

    starting sidebar

--- rsite/js/resume.jsx
+++ rsite/js/resume.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import ReactDOM, { render } from 'react-dom';
 import ResumeList from './resume_list';
+// import Sidebar from './resume_sidebar';
 
 class ResumePage extends React.Component {
   constructor(props) {
---
+++ rsite/js/resume_sidebar.jsx
@@ -0,0 +1,23 @@
+// view to contain resume options:
+// create
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // state attributes go here
+    };
+  }
+
+  render() {
+    return (
+      <div>foo</div>
+    );
+  }
+}
+
+Sidebar.propTypes = {};
+
+export default Sidebar;
