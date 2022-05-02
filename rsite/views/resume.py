bbf15d5a2afee4435dbdd25ab92864238db50b82
Thomas Dokas <dokastho@umich.edu>
2022-05-01 20:22:42 -0400

    add rest api route for loading resumes

--- rsite/views/resume.py
+++ rsite/views/resume.py
@@ -3,6 +3,8 @@ Site main resume content view.
 
 URLs include:
 /resume/
+/resume/load/
+/resume/new/
 """
 import flask
 import rsite
@@ -22,3 +24,31 @@ def show_resume():
             context["logname_link"] = f"/accounts/{logname}/"
 
     return flask.render_template('resume.html', **context)
+
+@rsite.app.route('/resume/load/')
+def load_resumes():
+    """Return resumes from database matching logname."""
+    with rsite.app.app_context():
+        logname = rsite.model.get_logname()
+        if not logname:
+            flask.abort(403)
+
+        database = rsite.model.get_db()
+        cur = database.execute(
+            "SELECT * "
+            "FROM reumes "
+            "WHERE owner == ?",
+            (logname, )
+        )
+        resumes = cur.fetchall()
+        data = {'resumes': []}
+        for res in resumes:
+            data['resumes'].append(
+                {
+                    "resumeid": res['resumeid'],
+                    "owner": logname,
+                    "content": res['content'],
+                    "img": res['snapshot']
+                }
+            )
+        return flask.jsonify(data), 201
