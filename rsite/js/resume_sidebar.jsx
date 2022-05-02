af5950f05e99b0f1720767c2ddf84c57ccb2263f
Thomas Dokas <dokastho@umich.edu>
2022-05-02 16:12:34 -0400

    fixed webpack config to facilitate new page

--- rsite/js/resume_sidebar.jsx
+++ rsite/js/resume_sidebar.jsx
@@ -2,8 +2,6 @@
 // create
 import React from 'react';
 // import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { Button } from 'semantic-ui-react';
 
 class Sidebar extends React.Component {
   constructor(props) {
@@ -16,11 +14,9 @@ class Sidebar extends React.Component {
 
   render() {
     return (
-      <Link to="/resume/new/">
-        <Button>
-          <p>Create a new resume</p>
-        </Button>
-      </Link>
+      <button type="button">
+        <a href="/resume/new">Create a new resume</a>
+      </button>
     );
   }
 }
