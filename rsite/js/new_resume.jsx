385635a1fea6b74c63b7534992d47e89e79466e1
Thomas Dokas <dokastho@umich.edu>
2022-05-03 11:34:09 -0400

    build-2022.05.03_11.34.09-tmp2

--- rsite/js/new_resume.jsx
+++ rsite/js/new_resume.jsx
@@ -35,8 +35,8 @@ class NewResume extends React.Component {
         ReactDOM.render(
           <div>
             <form action="/resume/?operation=create&target=/resume/" method="post" encType="multipart/form-data">
-              <input type="checkbox" name="type" value="Are you a student?" />
-              <input type="text" name="name" value="Resume name" />
+              <input type="checkbox" name="type" />
+              <input type="text" name="name" required />
               <input type="hidden" name="operation" value="create" />
             </form>
           </div>,
