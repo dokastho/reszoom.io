7bb7bc188e71c7db642c525f53c55b56cee4517b
Thomas Dokas <dokastho@umich.edu>
2022-05-03 18:33:45 -0400

    build-2022.05.03_18.33.45-tmp2

--- rsite/js/new_resume.jsx
+++ rsite/js/new_resume.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+// import '../static/css/styles.css';
 // import PropTypes from 'prop-types';
 
 class NewResume extends React.Component {
