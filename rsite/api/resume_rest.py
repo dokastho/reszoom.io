49d2ddbc36c11972c5c9dcc56e745faa04f3f698
Thomas Dokas <dokastho@umich.edu>
2022-05-04 02:46:08 -0400

    build-2022.05.04_02.46.08-tmp2

--- rsite/api/resume_rest.py
+++ rsite/api/resume_rest.py
@@ -40,8 +40,8 @@ def load_resumes():
                 (logname, )
             )
             entries = cur.fetchall()
-            if len(entries) == 0:
-                flask.abort(500)
+            # if len(entries) == 0:
+            #     flask.abort(500)
 
             # construct entries map
             data = {}
