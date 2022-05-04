122dc48451b6becae97a353fc5f54a18b4611dfa
Thomas Dokas <dokastho@umich.edu>
2022-05-03 23:40:42 -0400

    update view init

--- rsite/views/__init__.py
+++ rsite/views/__init__.py
@@ -4,4 +4,3 @@ from rsite.views.user import show_user
 from rsite.views.index import show_index
 from rsite.views.accounts import login, accounts
 from rsite.views.resume import show_resume, show_new, show_saved, post_resumes
-from rsite.views.entry import commit_entry
