53bb259ff0aff22b6aac1581697346f8dd6133f6
Thomas Dokas <dokastho@umich.edu>
2022-05-03 20:51:34 -0400

    allow for duplicating entryid

--- rsite/views/entry.py
+++ rsite/views/entry.py
@@ -26,35 +26,61 @@ def commit_entry():
     if op == "create":
         # get name and type of resume
         resumeid = flask.request.form.get('resumeid', type=int)
+        entryid = flask.request.form.get('entryid', type=int)
         content = flask.request.form.get('entrycontent')
         header = flask.request.form.get('header')
         
-        if len(content) == 0 or len(header) == 0:
+        if len(content) == 0 or len(header) == 0 or resumeid == 0:
             flask.abort(400)
-        
-        cur = database.execute(
+
+        # entryid not supplied, so entry is new
+        if entryid == None:
+            # insert new
+            cur = database.execute(
             "INSERT INTO entries "
             "(frequency, owner, header, content) "
             "VALUES (?, ?, ?, ?)",
             (1, logname, header, content, )
-        )
-        cur.fetchone()
+            )
+            cur.fetchone()
 
-        cur = database.execute(
-            "SELECT MAX(entryid) "
-            "AS id "
-            "FROM entries"
-        )
-        entryid = cur.fetchone()
-        entryid = entryid['id']
+            # get id
+            cur = database.execute(
+                "SELECT MAX(entryid) "
+                "AS id "
+                "FROM entries"
+            )
+            data = cur.fetchone()
+            entryid = data['id']
 
-        cur = database.execute(
-            "INSERT INTO resume_to_entry "
-            "(resumeid, entryid) "
-            "VALUES (?, ?)",
-            (resumeid, entryid)
-        )
-        cur.fetchone()
+            # insert id into resume_to_entry
+            cur = database.execute(
+                "INSERT INTO resume_to_entry "
+                "(resumeid, entryid) "
+                "VALUES (?, ?)",
+                (resumeid, entryid, )
+            )
+            cur.fetchone()
+        
+        else:
+            # entry already in db so increment freq
+            cur = database.execute(
+                "SELECT frequency "
+                "FROM entries "
+                "WHERE entryid == ?",
+                (entryid, )
+            )
+            data = cur.fetchone()
+            freq = data['frequency']
+            freq += 1
+
+            cur = database.execute(
+                "UPDATE entries "
+                "SET frequency = ? "
+                "WHERE entryid == ?",
+                (freq, entryid, )
+            )
+            cur.fetchone()
 
     elif op == "delete":
         pass
