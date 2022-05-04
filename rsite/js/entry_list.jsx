65fe052091562b24b67d233442f19f934dd260b3
Thomas Dokas <dokastho@umich.edu>
2022-05-04 14:34:00 -0400

    build-2022.05.04_14.34.00-tmp2

--- rsite/js/entry_list.jsx
+++ rsite/js/entry_list.jsx
@@ -78,11 +78,14 @@ class Entries extends React.Component {
       method: 'DELETE',
     }).then((response) => {
       if (!response.ok) throw Error(response.statusText);
-      this.setState((prevState) => (
-        {
-          entries: prevState.entries.delete(`${entryid}`),
-        }
-      ));
+      // eslint-disable-next-line prefer-arrow-callback
+      this.setState(function (prevState) {
+        prevState.entries.delete(`${entryid}`);
+        return {
+          entries: prevState.entries,
+          eids: prevState.eids.filter((eid) => eid.entryid !== entryid),
+        };
+      });
       // const { entries } = this.state;
       // console.log(entries);
     })
