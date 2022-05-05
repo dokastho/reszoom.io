f679f4c7c03fe006af97756caa56a5bb30452d17
Thomas Dokas <dokastho@umich.edu>
2022-05-05 16:57:41 -0400

    build-2022.05.05_16.57.41-tmp2

--- rsite/js/entry_list.jsx
+++ rsite/js/entry_list.jsx
@@ -64,17 +64,13 @@ class Entries extends React.Component {
       return response.json();
     }).then((data) => {
       this.setState((prevState) => ({
-        eids: prevState.eids.concat({
-          entryid: data.entryid,
-          pos: data.pos,
-          owner: username,
-          resumeid,
-        }),
+        eids: prevState.eids.concat(data.eid),
         entries: prevState.entries.set(`${data.entryid}`, data.entry),
         text: '',
       }));
     })
       .catch((error) => console.log(error));
+    console.log(username);
   }
 
   deleteEntry(entryid) {
