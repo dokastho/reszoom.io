580309b747d1a7f070314a4c30f23f700d624615
Thomas Dokas <dokastho@umich.edu>
2022-05-23 05:06:33 +0000

    build-2022.05.23_01.06.33-tmp2

--- rsite/js/resume_list.jsx
+++ rsite/js/resume_list.jsx
@@ -21,8 +21,10 @@ class ResumeList extends React.Component {
         </div>
         <div>
           {
-            resumes.length === 0 ? 'you have no resumes, create one'
-              : (
+            resumes.length === 0
+              ? (
+                <h3><a href="/resume/new">Create your first resume!</a></h3>
+              ) : (
                 resumes.map((r) => (
                   <div className="resume-link" key={r.resumeid}>
                     <p>
