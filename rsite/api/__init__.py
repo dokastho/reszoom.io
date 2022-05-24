2916368eaa127dc4e002e61a794dea124e7961b1
Thomas Dokas <dokastho@umich.edu>
2022-05-24 12:28:22 +0000

    build-2022.05.24_08.28.22-tmp2

--- rsite/api/__init__.py
+++ rsite/api/__init__.py
@@ -1,6 +1,5 @@
 """Site REST API."""
 from rsite.api.v1 import get_routes
-from rsite.api.user_rest import current_user
 from rsite.api.resume_rest import load_resumes
 from rsite.api.tag_rest import get_tags
 from rsite.api.entry_rest import post_entry, delete_entry, swap_entry, get_subentries, get_recommended
