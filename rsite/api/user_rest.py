22d13957001b0e6795ee91b0b24a6bc12cbfe968
Thomas Dokas <dokastho@umich.edu>
2022-05-24 13:53:54 +0000

    fetch not working?

---
+++ rsite/api/user_rest.py
@@ -0,0 +1,20 @@
+"""
+Site user rest api.
+
+URLs include:
+/api/v1/user/
+"""
+
+import flask
+import rsite
+from rsite.model import rest_api_auth_user
+
+@rsite.app.route("/api/v1/user/", methods=['POST'])
+def current_user():
+    """Return the logname of the current user."""
+
+    logname, _ = rest_api_auth_user()
+    
+    data = {'logname': logname}
+    
+    return flask.jsonify(data), 201
