14760982500213a7c022a2ada955b6a477c06691
Thomas Dokas <dokastho@umich.edu>
2022-05-28 14:58:44 +0000

    build-2022.05.28_10.58.44-tmp2

--- rsite/api/user_rest.py
+++ rsite/api/user_rest.py
@@ -7,7 +7,7 @@ URLs include:
 
 import flask
 import rsite
-from rsite.model import rest_api_auth_user
+from rsite.model import rest_api_auth_user, get_db
 
 @rsite.app.route("/api/v1/user/", methods=['GET'])
 def current_user():
@@ -27,4 +27,25 @@ def current_user():
     if data is None:
         flask.abort(500)
     
+    return flask.jsonify(data), 201
+
+
+@rsite.app.route("/api/v1/user/taken/<username>/", methods=['GET'])
+def username_taken(username):
+    """Return true if user exists."""
+    
+    database = get_db()
+    
+    cur = database.execute(
+        "SELECT * "
+        "FROM users "
+        'WHERE username == ?',
+        (username, )
+    )
+    
+    user = cur.fetchall()
+
+    data = {
+        "taken": len(user) != 0,
+    }
     return flask.jsonify(data), 201
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     new: './rsite/js/new_resume.jsx',
     edit: './rsite/js/builder.jsx',
     password: './rsite/js/password.jsx',
-    newpassword: './rsite/js/newpassword.jsx',
+    newpassword: './rsite/js/new_password.jsx',
   },
   output: {
     path: path.join(__dirname, '/rsite/static/js/'),
