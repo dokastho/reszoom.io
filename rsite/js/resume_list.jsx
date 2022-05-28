4c43b39f2f10c37142431db6cb80e656debc697d
Thomas Dokas <dokastho@umich.edu>
2022-05-28 15:57:22 +0000

    add dates to resume's

--- rsite/js/resume_list.jsx
+++ rsite/js/resume_list.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import EllipsisText from 'react-ellipsis-text';
+import Moment from 'moment';
 
 class ResumeList extends React.Component {
   constructor(props) {
@@ -34,20 +35,27 @@ class ResumeList extends React.Component {
                   <div>Desc</div>
                   <div className="spacer" />
                   <div>Tags</div>
+                  <div className="spacer" />
+                  <div>Date</div>
                 </div>
                 <hr />
                 {resumes.map((r) => (
                   <form action={`/resume/${r.resumeid}`} key={r.resumeid}>
                     <button type="submit" className="resume-link" background-color="white">
-                      <div>
+                      <div className="header">
                         {r.name}
                       </div>
                       <div className="spacer" />
-                      <div>
+                      <div className="header">
                         {r.description === null ? null
                           : <EllipsisText text={r.description} length={32} />}
                       </div>
                       <div className="spacer" />
+                      {/* <div className="header"></div> */}
+                      <div className="spacer" />
+                      <div className="header">
+                        {Moment(r.created).format('d MMM yy')}
+                      </div>
                     </button>
                   </form>
                 ))}
