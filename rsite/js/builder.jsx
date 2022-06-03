6d5df6f609c800a8b44ff287fcb22e103f8f608b
Thomas Dokas <dokastho@umich.edu>
2022-06-03 02:45:58 +0000

    build-2022.06.02_22.45.58-tmp2

--- rsite/js/builder.jsx
+++ rsite/js/builder.jsx
@@ -42,6 +42,7 @@ class ResumeBuilder extends React.Component {
     };
 
     this.renderSidebar = this.renderSidebar.bind(this);
+    // this.uniqueTags = this.uniqueTags.bind(this);
   }
 
   componentDidMount() {
@@ -152,7 +153,24 @@ class ResumeBuilder extends React.Component {
       .catch((error) => console.log(error));
   }
 
-  renderSidebar(tags) {
+  // uniqueTags() {
+  //   const { entries } = this.state;
+  //   const tagSet = [];
+  //   Object.keys(entries).forEach((entryid) => {
+  //     const entrytags = entries[entryid].tags;
+  //     if ('tags' in entries[entryid]) {
+  //       // tags only in the entry type
+  //       entrytags.forEach((tag) => {
+  //         if (!(tag in tagSet)) {
+  //           tagSet.push(tag);
+  //         }
+  //       });
+  //     }
+  //   });
+  //   return tagSet;
+  // }
+
+  renderSidebar() {
     const {
       resumetype,
       resumename,
@@ -160,6 +178,8 @@ class ResumeBuilder extends React.Component {
       username,
       filename,
     } = this.state;
+
+    // const tags = this.uniqueTags();
     const sidebar = document.getElementById('floating-sidebar');
 
     // construct the custom sidebar content
@@ -185,12 +205,12 @@ class ResumeBuilder extends React.Component {
         pagename={resumename}
         logname={username}
         content={sidebarContent}
-        tags={tags}
+        // tags={tags}
         img={filename}
       />,
       sidebar,
     );
-    this.setState({ tags });
+    // this.setState({ tags });
   }
 
   render() {
