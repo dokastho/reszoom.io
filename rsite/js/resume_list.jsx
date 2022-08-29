22103e8da0f3b07f17bffcfd9306f4bd713c6b33
Thomas Dokas <dokastho@umich.edu>
2022-08-29 15:51:58 +0000

    build-2022.08.29_11.51.58-tmp2

--- rsite/js/resume_list.jsx
+++ rsite/js/resume_list.jsx
@@ -30,38 +30,42 @@ class ResumeList extends React.Component {
             ) : (
               // TODO: table is a wip
               <table>
-                <tr>
-                  <th>Name</th>
-                  <th>Desc</th>
-                  <th>Tags</th>
-                  <th>Date</th>
-                </tr>
-                {resumes.map((r) => (
-                  <form action={`/resume/${r.resumeid}`} key={r.resumeid}>
-                    <tr onClick="submit">
-                      <button type="submit" className="resume-link" background-color="white">
-                        <td className="header">
-                          {r.name}
-                        </td>
-                        <td className="header">
-                          {r.description === null ? null
-                            : <EllipsisText text={r.description} length={32} />}
-                        </td>
-                        <td className="header">
-                          {
-                            // RENDER TAGS
-                            r.tags.map((t) => (
-                              <div className="tag" key={t}>{t}</div>
-                            ))
-                          }
-                        </td>
-                        <td className="header">
-                          {Moment(r.created).format('d MMM yy')}
-                        </td>
-                      </button>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Desc</th>
+                    <th>Tags</th>
+                    <th>Date</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {resumes.map((r, id) => (
+                    // <form action={`/resume/${r.resumeid}`} key={r.resumeid}>
+                    <tr key={id}>
+                      {/* <div role="button" tabIndex={0} className="entry-wrapper"> */}
+                      <td className="header">
+                        {r.name}
+                      </td>
+                      <td className="header">
+                        {r.description === null ? null
+                          : <EllipsisText text={r.description} length={32} />}
+                      </td>
+                      <td className="header">
+                        {
+                          // RENDER TAGS
+                          r.tags.map((t) => (
+                            <div className="tag" key={t}>{t}</div>
+                          ))
+                        }
+                      </td>
+                      <td className="header">
+                        {Moment(r.created).format('d MMM yy')}
+                      </td>
+                      {/* </div> */}
                     </tr>
-                  </form>
-                ))}
+                    // </form>
+                  ))}
+                </tbody>
               </table>
             )
         }
